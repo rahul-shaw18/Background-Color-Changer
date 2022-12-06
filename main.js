39d99194954016d8eaed1381b167b5456d708962
rahul-shaw18 <rahul.shaw505@gmail.com>
2022-12-06 23:02:21 +0530

    updated js code

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 function changeBG(colorName) {
+  document.body.style.color = "black";
   document.body.style.backgroundColor = colorName;
   if (colorName == "black") {
     document.body.style.color = "#fff";
-  } else {
-    document.body.style.color = "black";
   }
 }
